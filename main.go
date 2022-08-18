783410ec2f5682ece3cb730ef7cb814fe5531295
Ji Won Park <semanticist21@github.com>
2022-08-18 15:16:53 +0900

    test

--- main.go
+++ main.go
@@ -1,15 +1,8 @@
 package main
 
-import (
-	"fmt"
-	"net/http"
-)
+import "fmt"
 
 func main() {
-	http.ListenAndServe(":3030", nil)
-	fmt.Println(GetSunCoefficient(11.11))
-	fmt.Println(GetSunCoefficient(12.11))
-	fmt.Println(GetSunCoefficient(14.11))
-	fmt.Println(GetSunCoefficient(16.11))
-	fmt.Println(GetSunCoefficient(21.11))
+	fmt.Println("asd")
+	GetSunCoefficient(1.11)
 }
